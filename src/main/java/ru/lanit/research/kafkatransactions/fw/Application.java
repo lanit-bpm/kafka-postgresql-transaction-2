bb1739dd7f8ea42f561274157fd963ac87f0114c
Ермаков Денис <DEErmakov@lan.lanit.ru>
2022-03-21 12:28:20 +0300

    cleanup

--- src/main/java/ru/lanit/research/kafkatransactions/fw/Application.java
+++ src/main/java/ru/lanit/research/kafkatransactions/fw/Application.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// TODO: убрать явные указания packages в пользу defaults
 @SpringBootApplication(scanBasePackages = "ru.lanit")
 @EnableJpaRepositories("ru.lanit.research.kafkatransactions.adapter.jpa")
 @EntityScan("ru.lanit.research.kafkatransactions.domain")
@@ -17,11 +16,7 @@ public class Application {
 
     public static void main(String[] args) {
         var application = Application.createSpringApplication();
-
-        // Here we add the same initializer as we were using in our tests...
         application.addInitializers(new TestContainersInitializer.Initializer());
-
-        // ... and start it normally
         application.run(args);
     }
 
